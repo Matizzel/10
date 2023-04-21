4b07ad2552c54ccbe03b5ac509b6b1af9dc7bb50
matiz <matiz@MATIZZELLAPTOP>
2023-04-21 18:26:00 +0300

    Добавьте файлы проекта.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace _10
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            double x = double.Parse(textBox1.Text);
+            double y = double.Parse(textBox2.Text);
+            int n = int.Parse(textBox3.Text);
+            int k = int.Parse(textBox4.Text);
+            double z = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                for (int j = 1; j <= k; j++)
+                {
+                    z += (Math.Abs(Math.Pow(x, i)) + Math.Cos(y) + Math.Pow(y, i)) / ((i+1) * j);
+                }
+            }
+            textBox5.Text = z.ToString();
+        }
+    }
+}
+
